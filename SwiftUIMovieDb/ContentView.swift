eaecc3192613fc049de97232f5be7d02498156bc
BreksamAmr <85411193+BreksamAmr@users.noreply.github.com>
2022-07-31 11:44:12 +0200

    adding files

---
+++ SwiftUIMovieDb/ContentView.swift
@@ -0,0 +1,39 @@
+//
+//  ContentView.swift
+//  MovieApp
+//
+//  Created by Cofo on 28/07/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        TabView {
+            MovieListView()
+                .tabItem {
+                    VStack {
+                        Image(systemName: "tv")
+                        Text("Movies")
+                    }
+            }
+            .tag(0)
+            
+            MovieSearchView()
+                .tabItem {
+                    VStack {
+                        Image(systemName: "magnifyingglass")
+                        Text("Search")
+                    }
+            }
+            .tag(1)
+            
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
